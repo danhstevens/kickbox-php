9d3366a57016e3a283450ee6580b0693d2868d4e
Daniel Stevens <danhstevens@gmail.com>
2018-06-15 12:41:24 -0500

    adding trust response

--- src/Api/TrustApi.php
+++ src/Api/TrustApi.php
@@ -94,11 +94,12 @@ class TrustApi
      *
      * @throws \Kickbox\ApiException on non-2xx response
      * @throws \InvalidArgumentException
-     * @return void
+     * @return \Kickbox\Model\TrustValidateResponse
      */
     public function validate($app_id, $token, $email)
     {
-        $this->validateWithHttpInfo($app_id, $token, $email);
+        list($response) = $this->validateWithHttpInfo($app_id, $token, $email);
+        return $response;
     }
 
     /**
@@ -112,11 +113,11 @@ class TrustApi
      *
      * @throws \Kickbox\ApiException on non-2xx response
      * @throws \InvalidArgumentException
-     * @return array of null, HTTP status code, HTTP response headers (array of strings)
+     * @return array of \Kickbox\Model\TrustValidateResponse, HTTP status code, HTTP response headers (array of strings)
      */
     public function validateWithHttpInfo($app_id, $token, $email)
     {
-        $returnType = '';
+        $returnType = '\Kickbox\Model\TrustValidateResponse';
         $request = $this->validateRequest($app_id, $token, $email);
 
         try {
@@ -147,10 +148,32 @@ class TrustApi
                 );
             }
 
-            return [null, $statusCode, $response->getHeaders()];
+            $responseBody = $response->getBody();
+            if ($returnType === '\SplFileObject') {
+                $content = $responseBody; //stream goes to serializer
+            } else {
+                $content = $responseBody->getContents();
+                if ($returnType !== 'string') {
+                    $content = json_decode($content);
+                }
+            }
+
+            return [
+                ObjectSerializer::deserialize($content, $returnType, []),
+                $response->getStatusCode(),
+                $response->getHeaders()
+            ];
 
         } catch (ApiException $e) {
             switch ($e->getCode()) {
+                case 200:
+                    $data = ObjectSerializer::deserialize(
+                        $e->getResponseBody(),
+                        '\Kickbox\Model\TrustValidateResponse',
+                        $e->getResponseHeaders()
+                    );
+                    $e->setResponseObject($data);
+                    break;
                 case 400:
                     $data = ObjectSerializer::deserialize(
                         $e->getResponseBody(),
@@ -240,14 +263,28 @@ class TrustApi
      */
     public function validateAsyncWithHttpInfo($app_id, $token, $email)
     {
-        $returnType = '';
+        $returnType = '\Kickbox\Model\TrustValidateResponse';
         $request = $this->validateRequest($app_id, $token, $email);
 
         return $this->client
             ->sendAsync($request, $this->createHttpClientOption())
             ->then(
                 function ($response) use ($returnType) {
-                    return [null, $response->getStatusCode(), $response->getHeaders()];
+                    $responseBody = $response->getBody();
+                    if ($returnType === '\SplFileObject') {
+                        $content = $responseBody; //stream goes to serializer
+                    } else {
+                        $content = $responseBody->getContents();
+                        if ($returnType !== 'string') {
+                            $content = json_decode($content);
+                        }
+                    }
+
+                    return [
+                        ObjectSerializer::deserialize($content, $returnType, []),
+                        $response->getStatusCode(),
+                        $response->getHeaders()
+                    ];
                 },
                 function ($exception) {
                     $response = $exception->getResponse();
